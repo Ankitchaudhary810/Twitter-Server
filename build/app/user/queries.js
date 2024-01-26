3a9433e70468d755c8d238cc2b390c0367a19857
Ankit Chaudhary <ankitcha810@gmail.com>
2024-01-26 20:40:09 +0530

    initial commit for client and server

---
+++ build/app/user/queries.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.queries = void 0;
+exports.queries = `#graphql
+
+    verifyGoogleToken(token:String!):String
+    getCurrentUser:User
+    getOnlyName: String
+
+
+    DetectLoggedInUser(token:String!):User
+    
+`;
